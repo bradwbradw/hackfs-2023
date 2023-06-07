597fb29737d359c75f5f56774d1c1cab8909310c
bradwbradw <bradwbradw@xyz.gs>
2023-06-07 15:13:08 -0700

    any address

--- server/p2p.js
+++ server/p2p.js
@@ -12,7 +12,7 @@ async function startLibp2p() {
   const node = await createLibp2p({
     addresses: {
       // add a listen address (localhost) to accept TCP connections on a random port
-      listen: ['/ip4/127.0.0.1/tcp/0']
+      listen: ['/ip4/0.0.0.0/tcp/0']
     },
     transports: [tcp()],
     connectionEncryption: [noise()],
